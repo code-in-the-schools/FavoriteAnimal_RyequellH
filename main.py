7d832825712c31b14d1f2aa8f5a0303269ae170d
CodingQuell <ryequellhunter@gmail.com>
2020-07-22 15:30:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+Q = int(input("Enter any number 1-4 to learn about my favorite animal"))
+if(Q==1):
+  print("The color of my favorite aniamal is Blue")
+elif(Q==2): 
+  print("The sound of my animal can travel many miles across the ocean")
+elif(Q==3):
+  print("My aniamal can weigh up to 110,000 - 330,000 pounds")
+else:
+  print("My animal can eat up to 8,000 pounds of krill")
